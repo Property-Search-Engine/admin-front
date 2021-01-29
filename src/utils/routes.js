8faa400fc4e771da081e8e3cdc9ae6411b7d2a10
Roberto Llopis <robertollopis@msn.com>
2021-01-29 10:24:04 +0100

    Add router definition (#9)
    fixes #3 
    - Create utils router file 
    - Define one route for each view
    
    Co-authored-by: Roberto Llopis <roberto.llopis.developer@gmail.com>

---
+++ src/utils/routes.js
@@ -0,0 +1,10 @@
+const ROUTES = {
+    DASHBOARD: "/",
+    LOGIN: "/login",
+    SIGNUP: "/sign-up",
+    LISTING: "/listing",
+    HOME_DETAILS: "/listing/:id",
+    SETTINGS: "/settings",
+  };
+  
+  export default ROUTES;
