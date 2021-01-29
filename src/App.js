10db761880320d72e9d05a9dcc47544d8e1ad618
Roberto Llopis <roberto.llopis.developer@gmail.com>
2021-01-29 10:18:12 +0100

    Add router definition
    fixes #3 \n Create utils router file and define one route for each view

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 
-// import ROUTES from "./utils/routes";
+import ROUTES from "./utils/routes";
 
 function App() {
   useEffect(() => {}, []);
 
   return (
     <Switch>
-      <Route path={'/users'}>
-       <div>Hello world</div>
+      <Route path={ROUTES.HOME_DETAILS}>
+       <div>Property details</div>
+      </Route>
+      <Route path={ROUTES.LISTING}>
+       <div>Listing</div>
+      </Route>
+      <Route path={ROUTES.SETTINGS}>
+       <div>Settings</div>
+      </Route>
+      <Route path={ROUTES.DASHBOARD}>
+       <div>Dashboard</div>
       </Route>
     </Switch>
   );
