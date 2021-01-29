d1dde26622ccff551d44d26a57a024b82e8f92cb
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-01-29 16:15:23 +0100

    feature: Complete sidebar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
-import CardContainer from "./components/CardContainer";
-import Sidebar from "./components/common/Sidebar/Sidebar";
+import Dashboard from "./components/Dashboard/Dashboard"; 
 
 import ROUTES from "./utils/routes";
 
@@ -11,17 +10,16 @@ function App() {
   return (
     <Switch>
       <Route path={ROUTES.HOME_DETAILS}>
-       <div>Property details</div>
+          <div>Property details</div>
       </Route>
       <Route path={ROUTES.LISTING}>
-          <Sidebar active="listing"/>
           <div>Listing</div>
       </Route>
       <Route path={ROUTES.SETTINGS}>
-        <Sidebar active="settings"/>
+          <div>Settings</div>
       </Route>
       <Route path={ROUTES.DASHBOARD}>
-          <Sidebar active="dashboard"/>
+         <Dashboard/>
       </Route>
     </Switch>
   );
