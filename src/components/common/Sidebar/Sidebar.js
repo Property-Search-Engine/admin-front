ed1e1b1d93a6f32f8d10f727bf6be19c3c4084dc
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-01-29 16:31:49 +0100

    Fix type as per pull request comment

--- src/components/common/Sidebar/Sidebar.js
+++ src/components/common/Sidebar/Sidebar.js
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import { Nav, Image } from "react-bootstrap"; 
 import ROUTES from "../../../utils/routes"; 
-import NAVITEM from "../NavItem/NavItem"; 
+import NavItem from "../NavItem/NavItem"; 
 
 export default function Sidebar(props) {
 
@@ -10,10 +10,10 @@ export default function Sidebar(props) {
         <aside>
             <Nav className="sidebar flex-column" activeKey={ROUTES.DASHBOARD}>
                 <div className="sidebar-container flex-column justify-content-around d-flex">
-                    <NAVITEM src="/assets/icons/search.svg" href={ROUTES.DASHBOARD}/>
-                    <NAVITEM src="/assets/icons/dashboard.svg" text="Dashboard" href={ROUTES.DASHBOARD} active={active == "dashboard" ? true : false}/>
-                    <NAVITEM src="/assets/icons/home.svg" text="Listing" href={ROUTES.LISTING} active={active == "listing" ? true : false}/>
-                    <NAVITEM src="/assets/icons/settings.svg" text="Settings" href={ROUTES.SETTINGS} active={active == "settings" ? true : false}/>
+                    <NavItem src="/assets/icons/search.svg" href={ROUTES.DASHBOARD}/>
+                    <NavItem src="/assets/icons/dashboard.svg" text="Dashboard" href={ROUTES.DASHBOARD} active={active == "dashboard" ? true : false}/>
+                    <NavItem src="/assets/icons/home.svg" text="Listing" href={ROUTES.LISTING} active={active == "listing" ? true : false}/>
+                    <NavItem src="/assets/icons/settings.svg" text="Settings" href={ROUTES.SETTINGS} active={active == "settings" ? true : false}/>
                 </div>
             </Nav>
         </aside>
