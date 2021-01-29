8eee9195eb0a14313c51d9bcab33a826a0f43160
Roberto Llopis <roberto.llopis.developer@gmail.com>
2021-01-29 17:02:20 +0100

    Add React-modal

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,22 +1,36 @@
-import React from "react";
-import {Container, Row, Col, Card, Form, Button } from "react-bootstrap";
-import { withRouter } from "react-router";
+import React, { useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import Sidebar from "../common/Sidebar/Sidebar";
+import ModalForm from "../common/ModalForm/ModalForm";
 
-export default function Dashboard (props) {
-    return (
-        <>
-         <Container fluid>
-                <Row>
-                    <Col xs={2} id="sidebar-wrapper">      
-                      <Sidebar active="dashboard"/>
-                    </Col>
-                    <Col  xs={10} id="page-content-wrapper">
-                        this is a test
-                    </Col> 
-                </Row>
+export default function Dashboard(props) {
+  const [isModalFormOpen, setIsModalFormOpen] = useState(false);
 
-            </Container>
-        </>
-        );
-  };
+  function handleModalToggle() {
+    if (isModalFormOpen === true) {
+      setIsModalFormOpen(false);
+    } else {
+      setIsModalFormOpen(true);
+    }
+  }
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col xs={2} id="sidebar-wrapper">
+            <Sidebar active="dashboard" />
+          </Col>
+          <Col xs={10} id="page-content-wrapper">
+            <Button variant="primary" onClick={handleModalToggle}>
+              {isModalFormOpen ? "open" : "close"}
+            </Button>
+            <ModalForm
+              isModalFormOpen={isModalFormOpen}
+              handleModalToggle={handleModalToggle}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
