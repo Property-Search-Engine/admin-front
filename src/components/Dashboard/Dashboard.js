20ce0761e17b8eea2690315cc751609cf62d2099
Roberto Llopis <roberto.llopis.developer@gmail.com>
2021-01-31 13:22:02 +0100

    Unplug Modal Form in Dashboard to avoid conflicts

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,36 +1,10 @@
-import React, { useState } from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
-import Sidebar from "../common/Sidebar/Sidebar";
-import ModalForm from "../common/ModalForm/ModalForm";
+import React from "react";
 
 export default function Dashboard(props) {
-  const [isModalFormOpen, setIsModalFormOpen] = useState(false);
-
-  function handleModalToggle() {
-    if (isModalFormOpen === true) {
-      setIsModalFormOpen(false);
-    } else {
-      setIsModalFormOpen(true);
-    }
-  }
+  
   return (
     <>
-      <Container fluid>
-        <Row>
-          <Col xs={2} id="sidebar-wrapper">
-            <Sidebar active="dashboard" />
-          </Col>
-          <Col xs={10} id="page-content-wrapper">
-            <Button variant="primary" onClick={handleModalToggle}>
-              {isModalFormOpen ? "open" : "close"}
-            </Button>
-            <ModalForm
-              isModalFormOpen={isModalFormOpen}
-              handleModalToggle={handleModalToggle}
-            />
-          </Col>
-        </Row>
-      </Container>
+      
     </>
   );
 }
