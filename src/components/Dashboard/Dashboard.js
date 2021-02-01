157d4f629d45da9006c79e95edf024dbe3cde08f
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-02-01 12:25:15 +0100

    Complete search bar in header/navbar component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,22 +1,15 @@
 import React from "react";
-import {Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 import { withRouter } from "react-router";
 import Sidebar from "../common/Sidebar/Sidebar";
 import Header from "../common/Header/Header"; 
 
 export default function Dashboard (props) {
     return (
-        <main className="d-flex flex mainContainer">
-       
-              
-                         
-                      <Sidebar active="dashboard"/>
-    
-                        <div>
-                            <Header/>
-                        </div>
-            
-          
+        <main className="d-flex flex">
+            <Sidebar active="dashboard"/>
+            <div className="mainContainer">
+                <Header/>
+            </div>
         </main>
         );
   };
--- src/components/common/Sidebar/Sidebar.js
+++ src/components/common/Sidebar/Sidebar.js
@@ -1,5 +1,4 @@
 import React from 'react'; 
-import { Nav, Image, Col } from "react-bootstrap"; 
 import ROUTES from "../../../utils/routes"; 
 import NavItem from "../NavItem/NavItem"; 
 
@@ -10,9 +9,9 @@ export default function Sidebar(props) {
             <aside className="sidebar flex-column">
                     <div className="sidebar-container flex-column justify-content-around d-flex">
                         <NavItem src="/assets/icons/search.svg" href={ROUTES.DASHBOARD}/>
-                        <NavItem src="/assets/icons/dashboard.svg" text="Dashboard" href={ROUTES.DASHBOARD} active={active == "dashboard" ? true : false}/>
-                        <NavItem src="/assets/icons/home.svg" text="Listing" href={ROUTES.LISTING} active={active == "listing" ? true : false}/>
-                        <NavItem src="/assets/icons/settings.svg" text="Settings" href={ROUTES.SETTINGS} active={active == "settings" ? true : false}/>
+                        <NavItem src="/assets/icons/dashboard.svg" text="Dashboard" href={ROUTES.DASHBOARD} active={active === "dashboard" ? true : false}/>
+                        <NavItem src="/assets/icons/home.svg" text="Listing" href={ROUTES.LISTING} active={active === "listing" ? true : false}/>
+                        <NavItem src="/assets/icons/settings.svg" text="Settings" href={ROUTES.SETTINGS} active={active === "settings" ? true : false}/>
                     </div>
             </aside>
     )
