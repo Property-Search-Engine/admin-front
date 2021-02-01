192e6f2ba287239cbd317df46e4eee48b584e990
Sukaina <sukaina.ab220@gmail.com>
2021-02-01 12:49:50 +0100

    Some corections of curret branch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
-import CardContainer from "./components/CardContainer";
+
 import ListContainer from "../src/components/ListingContainer/ListContainer";
 
 import ROUTES from "./utils/routes";
@@ -11,21 +11,17 @@ function App() {
   return (
     <Switch>
       <Route path={ROUTES.HOME_DETAILS}>
-       <div>Property details</div>
+        <div>Property details</div>
       </Route>
       <Route path={ROUTES.LISTING}>
-       <div>
-         <ListContainer />
-       </div>
+        <div>
+          <ListContainer />
+        </div>
       </Route>
       <Route path={ROUTES.SETTINGS}>
-       <div>Settings</div>
+        <div>Settings</div>
       </Route>
-      <Route path={ROUTES.DASHBOARD}>
-       <div>Dashboard
-         <CardContainer/>
-       </div>
-      </Route>
+      <Route path={ROUTES.DASHBOARD}></Route>
     </Switch>
   );
 }
