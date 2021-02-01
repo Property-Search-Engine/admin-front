0452a12405536de6a747f68b65f85b8cf558cc32
Cris Garcia Mayoral <60959942+CrisGarciaM@users.noreply.github.com>
2021-02-01 12:34:27 +0100

    Feature/header component (#24)
    * Optimised sidebar styles
    
    * Complete search bar in header
    
    * Complete search bar in header/navbar component
    
    Co-authored-by: CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { withRouter } from "react-router";
+import Sidebar from "../common/Sidebar/Sidebar";
+import Header from "../common/Header/Header"; 
 
-export default function Dashboard(props) {
-  
-  return (
-    <>
-      
-    </>
-  );
-}
+export default function Dashboard (props) {
+    return (
+        <main className="d-flex flex">
+            <Sidebar active="dashboard"/>
+            <div className="mainContainer">
+                <Header/>
+            </div>
+        </main>
+        );
+  };
+
--- src/components/common/Sidebar/Sidebar.js
+++ src/components/common/Sidebar/Sidebar.js
@@ -1,5 +1,4 @@
 import React from 'react'; 
-import { Nav, Image } from "react-bootstrap"; 
 import ROUTES from "../../../utils/routes"; 
 import NavItem from "../NavItem/NavItem"; 
 
@@ -7,15 +6,13 @@ export default function Sidebar(props) {
 
     const { active } = props; 
     return (
-        <aside>
-            <Nav className="sidebar flex-column" activeKey={ROUTES.DASHBOARD}>
-                <div className="sidebar-container flex-column justify-content-around d-flex">
-                    <NavItem src="/assets/icons/search.svg" href={ROUTES.DASHBOARD}/>
-                    <NavItem src="/assets/icons/dashboard.svg" text="Dashboard" href={ROUTES.DASHBOARD} active={active == "dashboard" ? true : false}/>
-                    <NavItem src="/assets/icons/home.svg" text="Listing" href={ROUTES.LISTING} active={active == "listing" ? true : false}/>
-                    <NavItem src="/assets/icons/settings.svg" text="Settings" href={ROUTES.SETTINGS} active={active == "settings" ? true : false}/>
-                </div>
-            </Nav>
-        </aside>
+            <aside className="sidebar flex-column">
+                    <div className="sidebar-container flex-column justify-content-around d-flex">
+                        <NavItem src="/assets/icons/search.svg" href={ROUTES.DASHBOARD}/>
+                        <NavItem src="/assets/icons/dashboard.svg" text="Dashboard" href={ROUTES.DASHBOARD} active={active === "dashboard" ? true : false}/>
+                        <NavItem src="/assets/icons/home.svg" text="Listing" href={ROUTES.LISTING} active={active === "listing" ? true : false}/>
+                        <NavItem src="/assets/icons/settings.svg" text="Settings" href={ROUTES.SETTINGS} active={active === "settings" ? true : false}/>
+                    </div>
+            </aside>
     )
 }
