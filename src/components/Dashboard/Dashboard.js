0f14a47baee21f64282a1e42b916ec7405978637
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-02-02 13:28:46 +0100

    Delete html tags used for developing purposes

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -10,7 +10,6 @@ export default function Dashboard (props) {
             <Sidebar active="dashboard"/>
             <div className="mainContainer">
                 <Header/>
-                <ImageCarousel/>
             </div>
         </main>
         );
--- src/components/Listing/ImageCarousel/ImageCarousel.js
+++ src/components/Listing/ImageCarousel/ImageCarousel.js
@@ -1,14 +1,9 @@
 import React from 'react'; 
 import { Carousel } from "react-bootstrap"; 
-import ListContainer from "../ListingContainer/ListContainer"; 
 
 export default function ImageCarousel() {
 
     return (
-        <div className="listing-container">
-                <ListContainer whichView="details"/>
-            <div className="d-flex align-items-center justify-content-center w-100 p-10">
-                <div className="w-25 h-70 border">Info</div>
             <Carousel className="carousel-slider">
                 <Carousel.Item className="carousel-image">
                     <img className="d-block w-100" src="/assets/images/house1.jpg" alt="First slide"/>
@@ -20,8 +15,5 @@ export default function ImageCarousel() {
                     <img className="d-block w-100" src="/assets/images/house1.jpg" alt="Third slide"/>
                 </Carousel.Item>
             </Carousel>
-            </div>
-           
-        </div>
     )
 }
