d2ae404458060645f6503188195a55f1d7c03cc5
Cris Garcia Mayoral <60959942+CrisGarciaM@users.noreply.github.com>
2021-02-02 12:44:51 +0100

    Feature/header component (#27)
    * Optimised sidebar styles
    
    * Complete search bar in header
    
    * Complete search bar in header/navbar component
    
    * Complete notification and admin setting option in header/navbar
    
    Co-authored-by: CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
    Co-authored-by: Roberto Llopis <robertollopis@msn.com>

---
+++ src/components/common/Notification/Notification.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Notification() {
+    return (
+        <div>
+            <img className="notification-icon" src="/assets/icons/bell.svg"></img>
+        </div>
+    )
+}
