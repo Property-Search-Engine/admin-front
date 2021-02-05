6ee4c87326ba08d117ab26e7a36e6ede6ef2c9de
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-02-05 15:16:54 +0100

    Change new Admin form and added validation

--- src/components/pages/Settings/NewAdminForm/NewAdminForm.js
+++ src/components/pages/Settings/NewAdminForm/NewAdminForm.js
@@ -1,97 +1,77 @@
-import React, {useState} from 'react'; 
-import { Form, Button } from "react-bootstrap";
-import TextInput from "../../../common/Inputs/TextInput"; 
-import {svgPath} from "../../../../utils/helpers"; 
-import ROUTES from "../../../../utils/routes"; 
+import React from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
-export default function NewAdminForm() {
+ export default function SignupForm () {
+  return (
+    <Formik
+      initialValues={{ firstName: '', lastName: '', phoneNumber: '', email: '', password:'', }}
+      validationSchema=
+        {
+            Yup.object({
+                firstName: Yup.string()
+                .max(15, 'Must be 15 characters or less')
+                .required('Required'),
 
-    const initialFormData = {
-        firstName: "",
-        lastName: "",
-        phoneNumber: "",
-        email: "",
-        password: ""
-    }; 
+                lastName: Yup.string()
+                .max(20, 'Must be 20 characters or less')
+                .required('Required'),
 
-    const [formData, setformData] = useState(initialFormData); 
+                phoneNumber: Yup.string()
+                .min(9, 'Must be 9 numbers exactly')
+                .max(9, 'Must be 9 numbers exactly')
+                .required('Required'),
 
-    const {
-        firstName,
-        lastName,
-        phoneNumber,
-        email,
-        password
-    } = formData; 
+                email: Yup.string()
+                .email('Invalid email address')
+                .required('Required'),
 
-    const handleChange = (inputName, inputValue) => {
-        setformData({
-            ...formData, 
-            [inputName]: inputValue
-        });
-    }; 
+                password: Yup.string()
+                .min(6, 'Must be 6 numbers exactly')
+                .max(6, 'Must be 6 characters exactly')
+                .required('Required'),
+            })
+        }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          console.log(JSON.stringify(values, null, 2));     
+          setSubmitting(false);
+        }, 400);
+      }}
 
-        //POST to API
-        console.log(formData); 
-    };
-
-
-    return (
-            <Form id="settings-form" onSubmit={handleSubmit}>
-                
-                <div className="d-flex flex-column border p-3">
-                    <div className="heading">
-                        <h6>Create New Admin</h6>
-                    </div>
-                    <TextInput
-                    inputName="firstName"
-                    labelText="First Name"
-                    placeholder="First Name"
-                    labelImgSrc={svgPath("tagblue")}
-                    onChange={handleChange}
-                    value={firstName}
-                    />
-                    <TextInput
-                    inputName="lastName"
-                    labelText="Last Name"
-                    placeholder="Last Name"
-                    labelImgSrc={svgPath("tagblue")}
-                    onChange={handleChange}
-                    value={lastName}
-                    />
-                    <TextInput
-                    inputName="phoneNumber"
-                    labelText="Phone Number"
-                    placeholder="Phone Number"
-                    labelImgSrc={svgPath("tagblue")}
-                    onChange={handleChange}
-                    value={phoneNumber}
-                    />
-                    <TextInput
-                    inputName="email"
-                    labelText="Email"
-                    placeholder="Email"
-                    labelImgSrc={svgPath("tagblue")}
-                    onChange={handleChange}
-                    value={email}
-                    />
-                    <TextInput
-                    inputName="password"
-                    labelText="Password"
-                    placeholder="Password"
-                    labelImgSrc={svgPath("tagblue")}
-                    onChange={handleChange}
-                    value={password}
-                    />
-                    <div>
-                        <Button variant="primary" type="submit" className="mr-3 submitNewAdmin" onclick={handleSubmit}>
-                            Submit
-                        </Button>
-                    </div>
+    >
+      <Form id="settings-form" >
+        <div className="d-flex flex-column p-3">
+                <div className="heading">
+                    <h6>Create New Admin</h6>
                 </div>
-            </Form>
-    )
-}
+
+            <label htmlFor="firstName">First Name</label>
+            <Field name="firstName" type="text" />
+            <ErrorMessage component="div" name="firstName" className="errorMessage"/>
+
+            <label htmlFor="lastName">Last Name</label>
+            <Field name="lastName" type="text" />
+            <ErrorMessage component="div" name="lastName" className="errorMessage"/>
+
+            <label htmlFor="phoneNumber">Phone Number</label>
+            <Field name="phoneNumber" type="number" step="1"/>
+            <ErrorMessage component="div" name="phoneNumber" className="errorMessage"/>
+
+            <label htmlFor="email">Email Address</label>
+            <Field name="email" type="email" />
+            <ErrorMessage component="div" name="email" className="errorMessage"/>
+
+            <label htmlFor="password">Password</label>
+            <Field name="password" type="number" />
+            <ErrorMessage component="div" name="password" className="errorMessage"/>
+
+            <div className="submitNewAdmin">
+              <button type="submit" >Submit</button>
+            </div>
+        </div>
+      </Form>
+    </Formik>
+  );
+};
