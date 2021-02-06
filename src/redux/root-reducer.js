3af7ef8666493849157d782cf8b47d5313b80b58
Roberto Llopis <roberto.llopis.developer@gmail.com>
2021-02-06 22:02:30 +0100

    Create employees entity

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import UserReducer from "./user/user-reducer";
 import PropertiesReducer from "./properties/properties-reducer";
+import EmployeesReducer from "./employees/employees-reducer";
 
 const rootReducer = combineReducers({
   userState: UserReducer, // infoUser: {}
   propertiesState: PropertiesReducer, // infoProperties: {}
+  employeesState: EmployeesReducer, // infoEmployees: {}
 });
 
 export default rootReducer;
