79260519d8c64b8050b277d4b776f8a26f5019c0
Roberto Llopis <roberto.llopis.developer@gmail.com>
2021-02-06 22:02:58 +0100

    FormData, authHeaders makers and get employees route

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,7 +1,10 @@
 export const BASE_URL = "http://localhost:5000";
 
 export const userEndpoint = "/user";
+
 export const finalEndpoints = {
   register: BASE_URL + userEndpoint + "/register",
   login: BASE_URL + userEndpoint + "/login",
+  deleteEmployee: "/",
+  employeesList: "/",
 };
